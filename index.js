4e9c070dcf2e4a7f186c569487207ba9ce09ed0b
prakash <prakash@srivatech.com>
2018-12-05 02:22:49 +0530

    code added for post service calls

--- index.js
+++ index.js
@@ -18,11 +18,19 @@ couch.listDatabases().then((dbs)=>{
 console.log(dbs);
 });
 
-const dbname='sample';
-const viewurl='_design/all_customers/_view/all';
+const dbname='reviewsystem';
+const login='login';
+const loginurl='_design/login/_view/login';
+const feedbackdata="_design/login/_view/rv_feedback";
+
+app.use((req, res, next)=> {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/',(req,res)=>{
-    couch.get(dbname,viewurl).then((data,headers,status)=>{
+    couch.get(dbname,feedbackdata).then((data,headers,status)=>{
         res.send(data);
     },
     (err)=>{
@@ -41,6 +49,37 @@ app.post('/customer',(req,res)=>{
             name:name,
             age:age,
             location:location
+        }).then(
+            (data,headers,status)=>{
+                res.send(data);
+            },
+            (err)=>{
+                res.send(err);
+            }
+        );
+    });
+});
+
+app.post('/feedback',(req,res)=>{
+    couch.uniqid().then((ids)=>{
+        const id=ids[0];
+        couch.insert('reviewsystem',{
+        emp_name:req.body.emp_name,
+        emp_number:req.body.emp_number,
+        applicantname:req.body.applicantname,
+        position:req.body.position,
+        spocname:req.body.spocname,
+        rating:{
+            angular:req.body.rating.angular,
+            javascript:req.body.rating.javascript,
+            html:req.body.rating.html,
+            css:req.body.rating.css,
+            testingyool:req.body.rating.testingtool,
+            subversion:req.body.rating.subversion
+        },
+        comment:req.body.comment,
+        recommendation:req.body.recommendation,
+        status:req.body.status
         }).then(
             (data,headers,status)=>{
                 res.send(data);
